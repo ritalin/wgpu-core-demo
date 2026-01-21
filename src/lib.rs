49ebbc16200c2a1f1d48226e90d16eec67d6bc7f
Ritalin <kazu.ktz.tamura@gmail.com>
2026-01-21 14:54:59 +0900

    Fix chore

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
+use winit::event_loop::EventLoop;
 pub mod app;
+
+pub async fn run() -> Result<(), anyhow::Error> {
+    let mut app = app::App::new();
+    let event_loop = EventLoop::builder().build()?;
+
+    event_loop.run_app(&mut app)?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use wgpu_core_demo::app;
-use winit::event_loop::EventLoop;
+use wgpu_core_demo::run;
 
 fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
     pollster::block_on(run())
-}
-
-async fn run() -> Result<(), anyhow::Error> {
-    let mut app = app::App::new();
-    let event_loop = EventLoop::builder().build()?;
-
-    event_loop.run_app(&mut app)?;
-    Ok(())
 }
